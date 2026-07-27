53a1665ee5956f06e75aa4ad7ac17db6d24d315e
vinay3254 <vinaygk219@gmail.com>
2026-07-27 09:50:21 +0530

    ﻿Add SeqScan, Filter, and Project operators

---
+++ src/exec/project.rs
@@ -0,0 +1,42 @@
+use crate::error::ExecError;
+use crate::exec::Operator;
+use crate::storage::pager::Pager;
+use crate::types::value::Value;
+
+pub struct Project {
+    pub input: Box<dyn Operator>,
+    pub indices: Vec<usize>,
+}
+
+impl Operator for Project {
+    fn next(&mut self, pager: &mut Pager) -> Result<Option<Vec<Value>>, ExecError> {
+        match self.input.next(pager)? {
+            Some(row) => Ok(Some(self.indices.iter().map(|&i| row[i].clone()).collect())),
+            None => Ok(None),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct Fixed(Vec<Vec<Value>>);
+    impl Operator for Fixed {
+        fn next(&mut self, _pager: &mut Pager) -> Result<Option<Vec<Value>>, ExecError> {
+            Ok(self.0.pop())
+        }
+    }
+
+    #[test]
+    fn projects_selected_columns_in_order() {
+        use tempfile::NamedTempFile;
+        let file = NamedTempFile::new().unwrap();
+        let mut pager = crate::storage::pager::Pager::create(file.path()).unwrap();
+
+        let input = Fixed(vec![vec![Value::Integer(1), Value::Text("a".into()), Value::Boolean(true)]]);
+        let mut project = Project { input: Box::new(input), indices: vec![2, 0] };
+        let row = project.next(&mut pager).unwrap().unwrap();
+        assert_eq!(row, vec![Value::Boolean(true), Value::Integer(1)]);
+    }
+}
