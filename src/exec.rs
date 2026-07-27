69d253675d63a605014f73c028d09f7c256d8fe3
vinay3254 <vinaygk219@gmail.com>
2026-07-27 09:50:21 +0530

    ﻿Add SeqScan, Filter, and Project operators

--- src/exec.rs
+++ src/exec.rs
@@ -4,3 +4,11 @@ pub mod project;
 pub mod sort;
 pub mod limit;
 pub mod mutate;
+
+use crate::error::ExecError;
+use crate::storage::pager::Pager;
+use crate::types::value::Value;
+
+pub trait Operator {
+    fn next(&mut self, pager: &mut Pager) -> Result<Option<Vec<Value>>, ExecError>;
+}
