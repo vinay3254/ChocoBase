59249681de9d19e905ca936e3fdd323a28c1bdf6
vinay3254 <vinaygk219@gmail.com>
2026-08-20 00:24:42 +0530

    feat(http): update embedded studio bundle hash

--- src/http/dashboard.rs
+++ src/http/dashboard.rs
@@ -2,4 +2,4 @@
 
 pub const DASHBOARD_HTML: &str = include_str!("../../frontend/dist/index.html");
 pub const ASSET_CSS: &[u8] = include_bytes!("../../frontend/dist/assets/index-BCpDqGq7.css");
-pub const ASSET_JS: &[u8] = include_bytes!("../../frontend/dist/assets/index-C8ZwCaKj.js");
+pub const ASSET_JS: &[u8] = include_bytes!("../../frontend/dist/assets/index-CC-oh8s1.js");
